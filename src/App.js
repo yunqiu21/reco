8aecc434a12a76531ba8dbdeecd94cdeb6a7a158
FJXEF <eric42fang@gmail.com>
2021-02-19 22:36:07 -0800

    preparing to pull

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './App.css';
 import sample from './sample.jpg';
+import sample_drink from './drinks_sample.jpg'
 
 class Post extends React.Component {
-  constructor(props) {
+  constructor(props, image_name = sample) {
     super(props)
     this.state = {
       likes: 0,
       category: null,
+      image: image_name,
     }
   }
   render() {
     return (
       <div className="post">
         <img
-          src={sample}
+          src={sample_drink}
           alt="testing"
         />
         <p>Sample Picture</p>
@@ -44,11 +46,12 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      foods: [],
+      foods: [new Post('sample')],
       drinks: [],
       show_foods: true,
       show_drinks: true,
-      postArr: []
+      postArr: [],
+      upload: false
     }
   }
 
