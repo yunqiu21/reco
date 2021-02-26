bffd5915774fd14664b1e952c89d092f2237ab24
jasung <jason2137475@gmail.com>
2021-02-26 15:52:19 +0800

    added some backend functions that can be used later

---
+++ backend/App.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+
+//import routes
+const postsRoutes = require('./route/posts');
+
+app.use('/posts', postsRoutes);
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION, 
+    {useNewUrlParser: true},
+    () => {
+    console.log('Connected to DB');
+});
+
+
+app.listen(5000, () => {
+    console.log('Server running at port 5000');
+  });
---
+++ backend/route/posts.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const { reset } = require('nodemon');
+const router = express.Router();
+const Post = require('../models/Post');
+
+//GET BACK ALL POSTS
+router.get('/', async (req, res) => {
+    try{
+        const posts = await Post.find(); 
+        //const posts = await Post.find().limit   // this will return how many you want the DB
+        res.json(posts);
+    }catch(err){
+        res.json({message: err});
+    }
+ 
+  });
+
+router.get('/specific', async(req, res) => {
+    res.send('Hello, specific post!');
+  });
+
+  //SUBMIT A POST
+router.post('/', async(req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description:req.body.description
+    })
+    try{
+        const savedPost = await post.save()  //save to data base
+        res.json(savedPost);
+    }catch(err){
+        res.json({message: err});
+    }
+});
+
+
+//SPECIFIC POST
+router.get('/:postID', async(req, res) => {
+    try {  
+      const post = await Post.findById(req.params.postID);
+        res.json(post);
+    }catch{
+        res.json({message: err});
+    }
+} )
+
+//DELETE A SPECIFIC POST
+router.delete('/:postID', async(req, res) => {
+    try {  
+        const removedPost = await Post.remove({ _id:req.params.postID});
+        res.json(removedPost);
+    }catch{
+        res.json({message: err});
+    }
+} )
+
+//UPDATE A POST
+//DELETE A SPECIFIC POST
+router.patch('/:postID', async(req, res) => {
+    try {  
+        const updatedPost = await Post.updateOne(
+            { _id:req.params.postID}, 
+            {$set: { title: req.body.title}
+        });
+        res.json(updatedPost);
+    }catch{
+        res.json({message: err});
+    }
+} )
+
+module.exports = router;
