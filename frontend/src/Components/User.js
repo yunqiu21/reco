de6a89624cc9dd686cea11cdc935231d64e94d75
FJXEF <eric42fang@gmail.com>
2021-03-11 16:18:54 -0800

    storing to go back to temp

--- frontend/src/Components/User.js
+++ frontend/src/Components/User.js
@@ -1,36 +1,41 @@
+
 import React from "react";
 //import { useState } from 'react';
 import "./User.css"
 import eric from "../eric.jpg"
 //import axios from "axios";
 
-function User(){
+function User() {
 
-    let user = localStorage.getItem('userInfo');
-    user = JSON.parse(user);
-    console.log(user)
+  let user = localStorage.getItem('userInfo');
+  user = JSON.parse(user);
+  console.log(user)
 
-    let username = "you have not logged in yet!";
-    let signature = "";
-    if(localStorage.length != 0){
-      username = user.username;
-      signature = user.signature;
-    }
-    console.log(username);
+  let username = "you have not logged in yet!";
+  let signature = "";
+  if (localStorage.length != 0) {
+    username = user.username;
+    signature = user.signature;
+  }
+  console.log(username);
 
-    return (
-      <div className="profile">
-        <img src={eric} className="profile_pic" />
-        <div className="text">
-          <h1 className="username">{username}</h1>
-          <h2>{signature}</h2>
-          <div className="controlPanel">
-            <button className="button2">My Posts</button>
-            <a className="button2" href="/changesig">change signature</a>
-            <a className="button2" href="/changepwd">change password</a>
-          </div>
+  function logout() {
+    localStorage.clear();
+  }
+
+  return (
+    <div className="profile">
+      <img src={eric} className="profile_pic" />
+      <div className="text">
+        <h1 className="username">{username}</h1>
+        <h2>{signature}</h2>
+        <div className="controlPanel">
+          <button className="button2" onClick={() => logout()}>log out</button>
+          <a className="button2" href="/changesig">change signature</a>
+          <a className="button2" href="/changepwd">change password</a>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 export default User;
